1b8ab552dcb5bc5fdeb5f8bf6099a80127959264
junior lemos <juniorlemosoi@gmail.com>
2021-10-03 22:51:52 -0300

    Implementação das validações e criação das instancias

--- APIModelo/Startup.cs
+++ APIModelo/Startup.cs
@@ -28,10 +28,13 @@ namespace APIModelo
         {
 
             services.AddControllers();
+                
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIModelo", Version = "v1" });
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Modelo.Infra.Data/Context/MySqlContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Domain.Entities;
+using Modelo.Infra.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Infra.Data.Context
+{
+   public class MySqlContext :DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+                
+        
+        }
+    }
+}
